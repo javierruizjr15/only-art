b8fc01df747096258e5e268a008f4bec06401bac
Javier Ruiz <javierruizjr15@gmail.com>
2021-04-14 11:43:05 -0700

    db

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/blog_db', {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/artonly_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
