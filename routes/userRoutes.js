71473351ea59ec589bf151fccd378cbab49b7b17
Javier Ruiz <javierruizjr15@gmail.com>
2021-04-14 13:55:49 -0700

    c

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,3 +10,13 @@ router.post('/users/register', (req, res) => {
     res.sendStatus(200)
   })
 })
+
+
+
+
+//delete a user
+router.delete('/user/:id', passport.authenticate('jwt'), (req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.sendStatus(200))
+    .catch(err => res.json(err))
+})
