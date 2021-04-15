1a4bc2036e58bdbea9bb7011eca1d585b34cbd10
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-04-15 11:18:34 -0700

    code

---
+++ client/src/components/AppBar/AppBar.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import User from '../../utils/User'
+import './AppBar.css'
+
+const AppBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  const handleLogOut = () => {
+    localStorage.removeItem('user')
+    window.location = '/login'
+  }
+
+  useEffect(() => {
+    User.profile()
+      .then(() => setIsLoggedIn(true))
+      .catch(() => setIsLoggedIn(false))
+  }, [])
+  return (
+    <Navbar color='light' light expand='md'>
+      <Link to='/' className='link'>
+        <NavbarBrand>Not Facebook</NavbarBrand>
+      </Link>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className='mr-auto' navbar>
+          {
+            !isLoggedIn &&
+            <NavItem>
+              <Link to='/login' className='link'>
+                <NavLink>Register/Login</NavLink>
+              </Link>
+            </NavItem>
+          }
+          {
+            isLoggedIn &&
+            <>
+              <NavItem>
+                <Link to='/profile' className='link'>
+                  <NavLink>My Profile</NavLink>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to='/' className='link'>
+                  <NavLink>Home</NavLink>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <NavLink onClick={handleLogOut}>Log Out</NavLink>
+              </NavItem>
+            </>
+          }
+        </Nav>
+      </Collapse>
+    </Navbar>
+  )
+}
+
+export default AppBar
