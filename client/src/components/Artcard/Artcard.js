3f69755f7af49989c4c3cd9f11beebf2fa80f5c8
Javier Ruiz <javierruizjr15@gmail.com>
2021-04-19 10:37:30 -0700

    Changes to ArtCard cap 'C'

--- client/src/components/Artcard/Artcard.js
+++ client/src/components/Artcard/Artcard.js
@@ -3,7 +3,7 @@ import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
-import useFirestore from '../../utils/useFirestore.js'
+import useFirestore from '../../utils/useFirestore.js/index.js'
 import { motion } from 'framer-motion'
 
 const ArtCard = ({ setSelectedImg }) => {
